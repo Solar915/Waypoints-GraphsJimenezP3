5c16d7f82000886e866cbcccbdc865cb2ab8b886
Ethan Jimenez <145072456+Solar915@users.noreply.github.com>
2025-01-23 23:34:58 -0800

    god help me
    im stuck

--- P3JimenezWaypoints&Graphs/Assets/Scripts/FollowWP.cs
+++ P3JimenezWaypoints&Graphs/Assets/Scripts/FollowWP.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowWP : MonoBehaviour
-{
+public class FollowWP : MonoBehaviour {
 
     public GameObject[] waypoints;
     int currentWP = 0;
 
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
+    public float lookAhead = 10.0f;
 
     GameObject tracker;
 
@@ -23,7 +23,7 @@ public class FollowWP : MonoBehaviour
 
     void ProgressTracker()
     {
-        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3.0f)
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3)
         {
 
             currentWP++;
@@ -35,18 +35,17 @@ public class FollowWP : MonoBehaviour
             currentWP = 0;
 
             tracker.transform.LookAt(waypoints[currentWP].transform);
-            tracker.transform.Translate(0, 0, 0.1f);
+            tracker.transform.Translate(0, 0,(speed + 2) * Time.deltaTime);
         }
         // Update is called once per frame
         void Update()
         {
-            ProgressTracker();
 
-            //this.transform.LookAt(waypoints[currentWP].transform);
+            Quaternion lookAtWP = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
 
-            //Quaternion lookAtWP = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
-            //this.transform.rotation = Quaternion.Slerp(transform.rotation, lookAtWP, Time.deltaTime * rotSpeed);
-            //this.transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+            this.transform.rotation = Quaternion.Slerp(transform.rotation, lookAtWP, Time.deltaTime * rotSpeed);
+
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
         }
     }
 }
