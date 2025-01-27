1b8a5255bacfe5777c3679716d4cbeccbb84193e
Ethan Jimenez <145072456+Solar915@users.noreply.github.com>
2025-01-27 11:59:34 -0800

    ok
    k

--- P3JimenezWaypoints&Graphs/Assets/Scripts/FollowWP.cs
+++ P3JimenezWaypoints&Graphs/Assets/Scripts/FollowWP.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowWP : MonoBehaviour {
-
+public class FollowWP : MonoBehaviour
+{
     public GameObject[] waypoints;
     int currentWP = 0;
 
